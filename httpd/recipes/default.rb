bdab6475daa9f1cc6e1a82d41c666aff7c0cf223
萩原涼 <hagiwararyo@hagihararyounoMacBook-Pro.local>
2020-08-22 18:04:53 +0900

    とりあえずnginx

--- httpd/recipes/default.rb
+++ httpd/recipes/default.rb
@@ -3,11 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
-package "httpd" do
+package "nginx" do
     action :install
-  end
-  
-  # サービスの有効化と起動
-  service "httpd" do
-    action [ :enable, :start]
-  end
+end
